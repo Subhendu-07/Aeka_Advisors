960e899c39b0b9583fa8a2772fd7f18a914762dd
Subhendu-07 <mandalsubhendu243@gmail.com>
2026-01-04 17:11:28 +0530

    fix: configure CORS to allow specific origins for enhanced security

--- 1.creative-showcase/server/server.js
+++ 1.creative-showcase/server/server.js
@@ -12,7 +12,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://subhendu-creativeshowcase.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/images", imageRoutes);
